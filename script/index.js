5802154809d0d954b5ea4466eceb3acb9f69de81
RoyZiO <nziokinroy@gmail.com>
2022-12-20 20:18:20 +0300

    Adding functionality

--- script/index.js
+++ script/index.js
@@ -24,13 +24,14 @@ document.addEventListener('DOMContentLoaded', ()=>{
   })
 
 selectWorks = () =>{
-  dropDwn = document.getElementById("dropbtn")
+  //dropDwn = document.getElementById("dropbtn")
   image = document.getElementById('image_url')
   artTitle = document.getElementById('title')
   refNo = document.getElementById('main_reference_number')
   date = document.getElementById('date_display')
   artist = document.getElementById('artist_display')
-  work = document.querySelectorAll('a.list-group-item'); 
+  work = document.querySelectorAll('li.list-group-item'); 
+  const list = document.querySelectorAll('li.list-group-item');
 
 };
 selectWorks();
@@ -66,11 +67,35 @@ function openNav() {
 
 //Creating functions to open and suspend the sidebar on the right
 
-// fetch('https://api.npoint.io/fb6348ea0f63624394bb/data/')
-// .then(response => (response.json()))
-// .then(data=>{
 
-//  for (i= 1; i<11; i+=1) {
+
+
+fetch('https://api.npoint.io/fb6348ea0f63624394bb/data/')
+.then(response => (response.json()))
+.then( function(data) {
+    //console.log(data);
+
+        let i = -1
+        do {
+            i+=1
+
+        otherData = data.data[i];
+
+        work[i].textContent = otherData.title;
+
+        } while (i < 11)
+
+        //statement goes here
+        console.log(data[i]);
+       // work = data[i];
+
+    })
+
+        
+
+
+
+//  for (i= -1; i<11; i+=1) {
 //     otherData = data.data[i]
 
 //     //console.log(otherData);
@@ -78,22 +103,22 @@ function openNav() {
 //     work[i].textContent = otherData.title;
 //     console.log(work[i].textContent);
 //  }
-//  })
-// /
+ 
+//////////
 
-// const moreArtwork = document.querySelectorAll('a.list-group-item');
-// work.forEach (work => {
-//   work.addEventListener('click', (e) => {
-//     e.preventDefault()
-//     fetch('https://api.npoint.io/fb6348ea0f63624394bb/data/')
-//     .then(response => (response.json()))
-//       .then(data=> {
-//         moreData = data.data
-//         let i = Array.prototype.indexOf.call(work.parentNode.children, work);
-//         image.src = moreData[i].image_url 
-//         artTitle.textContent = moreData[i].title
-//         refNo.textContent = `MAIN REF. NO :   ${moreData[0].main_reference_number}`
-//         date.textContent = `Date of Display :   ${moreData[0].date_display}`
-//         artist.textContent = `Artist :  ${moreData[0].artist_display}`
+const moreArtwork = document.querySelectorAll('li.list-group-item');
+work.forEach (work => {
+  work.addEventListener('click', (e) => {
+    e.preventDefault()
+    fetch('https://api.npoint.io/fb6348ea0f63624394bb/data/')
+    .then(response => (response.json()))
+      .then(films=> {
+        moreData = films.data
+        let i = Array.prototype.indexOf.call(work.parentNode.children, work);
+        image.src = moreData[i].image_url 
+        artTitle.textContent = moreData[i].title
+        refNo.textContent = `MAIN REF. NO :   ${moreData[0].main_reference_number}`
+        date.textContent = `Date of Display :   ${moreData[0].date_display}`
+        artist.textContent = `Artist :  ${moreData[0].artist_display}`
 
-//       })})})
+      })})})
